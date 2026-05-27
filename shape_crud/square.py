c03788021fcaf3b559b2a71be798a50f9af500e2
david stern <dzs101010@gmail.com>
2026-05-27 07:41:30 +0300

    Correction in shapes and squares

--- shape_crud/square.py
+++ shape_crud/square.py
@@ -1,8 +1,8 @@
 from shape import Shape
 
 class Square(Shape):
-    def __init__(self, rid, id, shape_type):
-        super().__init__(id=1, shape_type="square")
+    def __init__(self, rid, shape_id, shape_type):
+        super().__init__(shape_id=1, shape_type="square")
         self.rid = rid
 
     def get_area(self):
